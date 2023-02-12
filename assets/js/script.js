639cdfad6bc0258e70c29b77582972f5b2394b4e
thorne7 <darienhallam@gmail.com>
2023-02-12 22:23:42 +1030

    fetching coordinates javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,67 @@
 var APIKey = "2a4d7eab71efe7b9e662e796cee38e75";
-
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cities + "&appid=" + APIKey;
-var cities = document.getElementById("citiesSearch").value
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+var cityName = document.getElementById("cityName").value;
+var searchButton = document.getElementById("searchButton");
 
 var lon = []
 var lat = []
 
 
 
-$("#searchBtn").click (function () {
-        console.log("click");
+
+function fetchCoordinates(cityName, APIKey) {
+    const APIUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`;
+    return fetch(APIUrl)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(`API request failed with status code: ${response.status}`);
+        }
+      })
+      .then(data => {
+        if (data.coord) {
+          return {
+            longitude: data.coord.lon,
+            latitude: data.coord.lat
+            temperature: data.main.temp,
+            windSpeed: data.wind.speed,
+            humidity: data.main.humidity
+          };
+        } else {
+          throw new Error('Coordinates not found in the API response');
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  
+  searchButton.onclick = function() {
+    const cityName = document.getElementById('cityName').value;
+    const APIKey = "2a4d7eab71efe7b9e662e796cee38e75";
+  
+    fetchCoordinates(cityName, APIKey)
+      .then(coordinates => {
+        console.log(coordinates);
+      });
+  };
 
 
-        var searchCity = $("citiesSearch").val();
-        console.log(searchCity);
-        if (searchCity === "") {
-            return;
-        }})
+
+
+
+
+
+// $("#searchBtn").click (function () {
+//         console.log("click");
+
+
+//         var searchCity = $("citiesSearch").val();
+//         console.log(searchCity);
+//         if (searchCity === "") {
+//             return;
+//         }})
 
 
 
