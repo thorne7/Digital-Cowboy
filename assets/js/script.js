f8ea145ed2a38fb94a9583cf9dd9c64cdcdb0cdf
thorne7 <darienhallam@gmail.com>
2023-03-07 15:19:56 +1030

    getting forecast data as object in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,49 +6,51 @@ const todayWeather = document.getElementById("todayWeather");
 const tempEl = document.getElementById("Temperature");
 const windEl = document.getElementById("Wind");
 const humidityEl = document.getElementById("Humidity");
-const forecastList = document.getElementById('forecastList');
+const forecastList = document.getElementById("forecastList");
+const cityElName = document.getElementById("city");
+const previousCities = JSON.parse(localStorage.getItem("previousCities")) || [];
 
-var cityElName = document.getElementById("city");
-
-
-let previousCities = JSON.parse(localStorage.getItem("previousCities")) || [];
-
-async function fetchWeatherData(cityName, weatherAPIKey) {
+function fetchWeatherData(cityName, weatherAPIKey) {
   const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${weatherAPIKey}`;
-  const response = await fetch(APIUrl);
-  if (response.ok) {
-    const data = await response.json();
-    return {
-      longitude: data.coord.lon,
-      latitude: data.coord.lat,
-      temperature: data.main.temp,
-      windSpeed: data.wind.speed,
-      humidity: data.main.humidity,
-    };
-  } else {
-    throw new Error(`API request failed with status code: ${response.status}`);
-  }
+  return fetch(APIUrl)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`API request failed with status code: ${response.status}`);
+      }
+    })
+    .then(data => {
+      return {
+        longitude: data.coord.lon,
+        latitude: data.coord.lat,
+        temperature: data.main.temp,
+        windSpeed: data.wind.speed,
+        humidity: data.main.humidity,
+      };
+    });
 }
 
-async function fetchForecastData(latitude, longitude, forecastAPIKey) {
+function fetchForecastData(latitude, longitude, forecastAPIKey) {
   const APIUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${forecastAPIKey}&cnt=5`;
-  const response = await fetch(APIUrl);
-  if (response.ok) {
-    const data = await response.json();
-    return data.list;
-  } else {
-    throw new Error(`API request failed with status code: ${response.status}`);
-  }
-  console.log(forecastData)
+  return fetch(APIUrl)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`API request failed with status code: ${response.status}`);
+      }
+    })
+    .then(data => {
+      return data.list;
+    });
 }
 
 
 function updateWeatherUI(weatherData, forecastData) {
-  // cityElName.innerHTML = `Location: ${cityNameInput}`;
   tempEl.innerHTML = `Temperature: ${weatherData.temperature} °C`;
   windEl.innerHTML = `Wind speed: ${weatherData.windSpeed} m/s`;
   humidityEl.innerHTML = `Humidity: ${weatherData.humidity} %`;
-  console.log(forecastData)
   updateForecastUI(forecastData);
 }
 
@@ -67,25 +69,45 @@ function updateForecastUI(forecastData) {
     return nextFiveDays.includes(date) && hour === '12';
   });
 
+  // group the forecasts by day
+  const groupedForecastData = {};
   filteredForecastData.forEach(forecast => {
-    const forecastDate = dayjs(forecast.dt_txt).format('ddd, MMM D');
-    const forecastTemp = forecast.main.temp;
-    const forecastIconUrl = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`;
-    const forecastWindSpeed = forecast.wind.speed;
-    const forecastHumidity = forecast.main.humidity;
-
-    const forecastEl = document.createElement('li');
-    forecastEl.innerHTML = `
-      <div class="forecast-item">
-        <span class="forecast-date">${forecastDate}</span>
-        <img src="${forecastIconUrl}" class="forecast-icon"/>
-        <span class="forecast-temp">${forecastTemp} °C</span>
-        <span class="forecast-wind-speed">${forecastWindSpeed} m/s</span>
-        <span class="forecast-humidity">${forecastHumidity} %</span>
-      </div>
-    `;
-    forecastList.appendChild(forecastEl);
+    const forecastDay = dayjs(forecast.dt_txt).format('dddd');
+    if (!groupedForecastData[forecastDay]) {
+      groupedForecastData[forecastDay] = [];
+    }
+    groupedForecastData[forecastDay].push(forecast);
   });
+
+  // create a forecast item for each day
+  Object.entries(groupedForecastData).forEach(([day, forecasts]) => {
+    const dayEl = document.createElement('div');
+    dayEl.classList.add('forecast-day');
+    dayEl.innerHTML = `<h3>${day}</h3>`;
+    forecastList.appendChild(dayEl);
+
+    forecasts.forEach(forecast => {
+      const forecastDate = dayjs(forecast.dt_txt).format('HH:mm');
+      const forecastTemp = forecast.main.temp;
+      const forecastIconUrl = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`;
+      const forecastWindSpeed = forecast.wind.speed;
+      const forecastHumidity = forecast.main.humidity;
+
+      const forecastEl = document.createElement('div');
+      forecastEl.classList.add('forecast-item');
+      forecastEl.innerHTML = `
+        <div class="forecast-time">${forecastDate}</div>
+        <div class="forecast-info">
+          <img src="${forecastIconUrl}" class="forecast-icon"/>
+          <div class="forecast-temp">${forecastTemp} °C</div>
+          <div class="forecast-wind-speed">${forecastWindSpeed} m/s</div>
+          <div class="forecast-humidity">${forecastHumidity} %</div>
+        </div>
+      `;
+      dayEl.appendChild(forecastEl);
+    });
+  });
+  console.log(forecastData);
 }
 
 
@@ -99,6 +121,7 @@ function saveCityToLocalStorage(cityName) {
     localStorage.setItem("previousCities", JSON.stringify(previousCities));
 
     // Create a button for the new city and add it to the UI
+
     const buttonEl = document.createElement("button");
     buttonEl.classList.add("btn-primary");
     buttonEl.innerHTML = cityName;
@@ -111,15 +134,16 @@ function saveCityToLocalStorage(cityName) {
 }
 
 function updateCityButtonsSection() {
-  const previousCitiesList = document.querySelector('#previousCities');
-  previousCitiesList.innerHTML = '';
+
+  const previousCitiesList = document.querySelector("#previousCities");
+  previousCitiesList.innerHTML = "";
   for (let i = 0; i < previousCities.length; i++) {
     const city = previousCities[i];
-    const cityButton = document.createElement('button');
+    const cityButton = document.createElement("button");
     cityButton.textContent = city;
 
-    cityButton.classList.add('btn', 'btn-primary', 'mx-1', 'mb-2');
-    cityButton.addEventListener('click', () => {
+    cityButton.classList.add("btn", "btn-primary", "mx-1", "mb-2");
+    cityButton.addEventListener("click", () => {
       fetchWeatherData(city, weatherAPIKey)
         .then(weatherData => {
           fetchForecastData(weatherData.latitude, weatherData.longitude, forecastAPIKey)
@@ -130,38 +154,28 @@ function updateCityButtonsSection() {
     });
     previousCitiesList.appendChild(cityButton);
 
-    const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = '&times;';
-    deleteButton.classList.add('btn', 'btn-danger', 'mx-1', 'mb-2');
-    deleteButton.addEventListener('click', () => {
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "&times;";
+    deleteButton.classList.add("btn", "btn-danger", "mx-1", "mb-2");
+    deleteButton.addEventListener("click", () => {
       previousCities.splice(i, 1);
-      localStorage.setItem('previousCities', JSON.stringify(previousCities));
+      localStorage.setItem("previousCities", JSON.stringify(previousCities));
       updateCityButtonsSection();
     });
     previousCitiesList.appendChild(deleteButton);
   }
 }
 
-
-// Initialize the page with the weather and forecast data for the first city in the previousCities array
-if (previousCities.length > 0) {
-  const firstCityName = previousCities[0];
-  document.getElementById("cityName").value = firstCityName;
-  searchButton.click();
-}
-
 searchButton.addEventListener("click", async () => {
-  const cityName = cityNameInput.value.trim();
   try {
+    const cityName = cityNameInput.value;
     const weatherData = await fetchWeatherData(cityName, weatherAPIKey);
     const forecastData = await fetchForecastData(weatherData.latitude, weatherData.longitude, forecastAPIKey);
     updateWeatherUI(weatherData, forecastData);
-    updateForecastUI(forecastData);
     saveCityToLocalStorage(cityName);
     updateCityButtonsSection();
   } catch (error) {
-    console.error(error);
-    alert("Failed to fetch weather data. Please check the city name and try again.");
+    console.log(error);
   }
 });
 
